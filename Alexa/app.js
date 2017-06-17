5eaa6a9531bcc5648a00437a24e58bd934894860
Shreeyam Kacker <Shreeyam Kacker>
2017-06-17 11:27:32 +0100

    Added samples and deleted old build name

--- Alexa/app.js
+++ Alexa/app.js
@@ -2,7 +2,9 @@
 // Team DroneSoc
 // Written by Shreeyam Kacker, 2017-06-17
 
-const Alexa = require('alexa-sdk');
+const Alexa = require('alexa-sdk'),
+        extensions = require('/extensions'),
+        appId = 'amzn1.ask.skill.a8e13fd4-59cd-468f-ba76-724309587de3'; 
 
 // Handler for intents
 const handlers = {
@@ -14,6 +16,7 @@ const handlers = {
 // Exports handler, called by Lambda
 exports.handler = (event, context, callback) => {
     var alexa = Alexa.handler(event, context, callback);
+    alexa.appId = appId;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
